ad16c60a2a301a76edc505b0db1736e583956596
meresa16 <meresagidey0938@gmail.com>
2025-12-17 09:22:02 +0300

    task updated

--- my_dbt_project/models/staging/stg_crypto.sql
+++ my_dbt_project/models/staging/stg_crypto.sql
@@ -1,9 +1,34 @@
+-- SELECT
+--     id as coin_id,
+--     symbol,
+--     current_price as price_usd,
+--     market_cap,
+--     CAST(loaded_at as DATE) as report_date
+-- FROM {{ source('crypto_raw', 'daily_market') }}
+-- -- This deduplicates data if we ran the script twice in one day
+-- QUALIFY ROW_NUMBER() OVER (PARTITION BY id, CAST(loaded_at as DATE) ORDER BY loaded_at DESC) = 1
+
+
+
+
+
+{{
+    config(
+        materialized='incremental',
+        unique_key='coin_id' 
+    )
+}}
+
 SELECT
     id as coin_id,
     symbol,
     current_price as price_usd,
     market_cap,
+    last_updated,
     CAST(loaded_at as DATE) as report_date
 FROM {{ source('crypto_raw', 'daily_market') }}
--- This deduplicates data if we ran the script twice in one day
-QUALIFY ROW_NUMBER() OVER (PARTITION BY id, CAST(loaded_at as DATE) ORDER BY loaded_at DESC) = 1
+
+{% if is_incremental() %}
+  -- If this runs incrementally, only pick up data newer than what we already have
+  WHERE loaded_at > (SELECT MAX(report_date) FROM {{ this }})
+{% endif %}
