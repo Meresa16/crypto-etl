49c9b9dc2e14a0bf61a4180b9ea4e90238d07b46
Meresa Gidey <71267586+Meresa16@users.noreply.github.com>
2025-12-18 09:52:45 +0300

    Update ingest.py

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -1,75 +1,5 @@
 
 
-# import pandas as pd
-# import pandas_gbq # Make sure to install this in requirements.txt
-# import requests
-# import os
-# import json
-# from datetime import datetime
-# from google.oauth2 import service_account
-
-# # --- CONFIGURATION ---
-# PROJECT_ID = 'loyal-weaver-471905-p9' 
-# DATASET_ID = 'crypto_raw'
-# TABLE_ID = 'daily_market'
-# CREDENTIALS_PATH = './gcp_key.json'
-
-# # 1. EXTRACT
-# print("Extracting data...")
-# url = "https://api.coingecko.com/api/v3/coins/markets"
-# params = {
-#     'vs_currency': 'usd',
-#     'order': 'market_cap_desc',
-#     'per_page': 50,
-#     'page': 1
-# }
-
-# try:
-#     response = requests.get(url, params=params)
-#     response.raise_for_status()
-#     data = response.json()
-# except Exception as e:
-#     print(f"API Error: {e}")
-#     exit(1)
-
-# # 2. TRANSFORM
-# df = pd.DataFrame(data)
-# df = df[['id', 'symbol', 'name', 'current_price', 'market_cap', 'total_volume', 'last_updated']]
-# df['loaded_at'] = datetime.now()
-
-# # 3. AUTHENTICATION (With Debugging)
-# print(f"Loading to BigQuery Project: {PROJECT_ID}...")
-
-# credentials = None
-# if os.path.exists(CREDENTIALS_PATH):
-#     try:
-#         credentials = service_account.Credentials.from_service_account_file(CREDENTIALS_PATH)
-#         print("✅ Credentials loaded successfully.")
-#     except json.JSONDecodeError:
-#         print("❌ CRITICAL ERROR: The gcp_key.json file is corrupt.")
-#         print("This usually means the GitHub Secret is missing quotes.")
-#         # Print the first 20 chars to show the format (safe, doesn't leak full key)
-#         with open(CREDENTIALS_PATH, 'r') as f:
-#             print(f"File preview: {f.read(50)}...") 
-#         exit(1)
-# else:
-#     print("⚠️ Warning: No 'gcp_key.json' found. Using default environment...")
-
-# # 4. LOAD (Using the modern library)
-# try:
-#     pandas_gbq.to_gbq(
-#         df,
-#         destination_table=f"{DATASET_ID}.{TABLE_ID}",
-#         project_id=PROJECT_ID,
-#         if_exists='append',
-#         credentials=credentials,
-#     )
-#     print("Success! Data loaded.")
-# except Exception as e:
-#     print(f"BigQuery Load Error: {e}")
-#     exit(1)
-
-
 
 
 
@@ -93,7 +23,7 @@ url = "https://api.coingecko.com/api/v3/coins/markets"
 params = {
     'vs_currency': 'usd',
     'order': 'market_cap_desc',
-    'per_page': 50,
+    'per_page': 500,
     'page': 1
 }
 headers = {
